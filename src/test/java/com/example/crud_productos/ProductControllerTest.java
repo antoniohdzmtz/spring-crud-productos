8012cec240b8f0126d34e1f80ecfef94b6c0a354
Antonio <antonio.hernandez>
2025-06-19 20:38:59 -0600

    se sube prueba unitaria

--- src/test/java/com/example/crud_productos/ProductControllerTest.java
+++ src/test/java/com/example/crud_productos/ProductControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
