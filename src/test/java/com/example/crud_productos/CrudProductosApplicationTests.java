b63fbc7ef16721a856883540507e583afc689d99
Antonio <antonio.hernandez>
2025-06-19 19:26:52 -0600

    correccion

--- src/test/java/com/example/crud_productos/CrudProductosApplicationTests.java
+++ src/test/java/com/example/crud_productos/CrudProductosApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.crud_productos;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CrudProductosApplicationTests {
 
 	@Test
